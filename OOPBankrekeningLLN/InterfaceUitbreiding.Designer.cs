4faee55a9cedd29c11438b18ef137538e455aaf7
Jeff Nuyts <NUYJE@sintcordula.be>
2024-10-09 11:41:22 +0200

    Add project files.

---
+++ OOPBankrekeningLLN/InterfaceUitbreiding.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace OOPBankrekeningLLN
+{
+    partial class InterfaceUitbreiding
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpname = new System.Windows.Forms.Button();
+            this.btnStorting = new System.Windows.Forms.Button();
+            this.txtBedrag = new System.Windows.Forms.TextBox();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.lblNaam = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNieuweRekening = new System.Windows.Forms.Button();
+            this.txtAN = new System.Windows.Forms.TextBox();
+            this.txtVN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnNieuweKlant = new System.Windows.Forms.Button();
+            this.lstKlanten = new System.Windows.Forms.ListBox();
+            this.lstRekeningen = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRekening = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpname
+            // 
+            this.btnOpname.Location = new System.Drawing.Point(644, 255);
+            this.btnOpname.Name = "btnOpname";
+            this.btnOpname.Size = new System.Drawing.Size(75, 23);
+            this.btnOpname.TabIndex = 23;
+            this.btnOpname.Text = "Opname";
+            this.btnOpname.UseVisualStyleBackColor = true;
+            
+            // 
+            // btnStorting
+            // 
+            this.btnStorting.Location = new System.Drawing.Point(478, 255);
+            this.btnStorting.Name = "btnStorting";
+            this.btnStorting.Size = new System.Drawing.Size(75, 23);
+            this.btnStorting.TabIndex = 22;
+            this.btnStorting.Text = "Storting";
+            this.btnStorting.UseVisualStyleBackColor = true;
+            
+            // 
+            // txtBedrag
+            // 
+            this.txtBedrag.Location = new System.Drawing.Point(606, 203);
+            this.txtBedrag.Name = "txtBedrag";
+            this.txtBedrag.Size = new System.Drawing.Size(100, 20);
+            this.txtBedrag.TabIndex = 21;
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Location = new System.Drawing.Point(603, 175);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(0, 13);
+            this.lblSaldo.TabIndex = 20;
+            // 
+            // lblNaam
+            // 
+            this.lblNaam.AutoSize = true;
+            this.lblNaam.Location = new System.Drawing.Point(603, 141);
+            this.lblNaam.Name = "lblNaam";
+            this.lblNaam.Size = new System.Drawing.Size(0, 13);
+            this.lblNaam.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(475, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Bedrag";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(475, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Saldo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(475, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Naam";
+            // 
+            // btnNieuweRekening
+            // 
+            this.btnNieuweRekening.Location = new System.Drawing.Point(250, 102);
+            this.btnNieuweRekening.Name = "btnNieuweRekening";
+            this.btnNieuweRekening.Size = new System.Drawing.Size(185, 23);
+            this.btnNieuweRekening.TabIndex = 15;
+            this.btnNieuweRekening.Text = "nieuwe rekening aanmaken";
+            this.btnNieuweRekening.UseVisualStyleBackColor = true;
+            
+            // 
+            // txtAN
+            // 
+            this.txtAN.Location = new System.Drawing.Point(107, 65);
+            this.txtAN.Name = "txtAN";
+            this.txtAN.Size = new System.Drawing.Size(100, 20);
+            this.txtAN.TabIndex = 14;
+            // 
+            // txtVN
+            // 
+            this.txtVN.Location = new System.Drawing.Point(107, 33);
+            this.txtVN.Name = "txtVN";
+            this.txtVN.Size = new System.Drawing.Size(100, 20);
+            this.txtVN.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Voornaam";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Familienaam";
+            // 
+            // btnNieuweKlant
+            // 
+            this.btnNieuweKlant.Location = new System.Drawing.Point(39, 102);
+            this.btnNieuweKlant.Name = "btnNieuweKlant";
+            this.btnNieuweKlant.Size = new System.Drawing.Size(168, 23);
+            this.btnNieuweKlant.TabIndex = 25;
+            this.btnNieuweKlant.Text = "nieuwe klant toevoegen";
+            this.btnNieuweKlant.UseVisualStyleBackColor = true;
+            
+            // 
+            // lstKlanten
+            // 
+            this.lstKlanten.FormattingEnabled = true;
+            this.lstKlanten.Location = new System.Drawing.Point(39, 141);
+            this.lstKlanten.Name = "lstKlanten";
+            this.lstKlanten.Size = new System.Drawing.Size(168, 199);
+            this.lstKlanten.TabIndex = 26;
+            // 
+            // lstRekeningen
+            // 
+            this.lstRekeningen.FormattingEnabled = true;
+            this.lstRekeningen.Location = new System.Drawing.Point(250, 141);
+            this.lstRekeningen.Name = "lstRekeningen";
+            this.lstRekeningen.Size = new System.Drawing.Size(185, 199);
+            this.lstRekeningen.TabIndex = 27;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(250, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Naam rekening";
+            // 
+            // txtRekening
+            // 
+            this.txtRekening.Location = new System.Drawing.Point(335, 65);
+            this.txtRekening.Name = "txtRekening";
+            this.txtRekening.Size = new System.Drawing.Size(100, 20);
+            this.txtRekening.TabIndex = 29;
+            // 
+            // InterfaceUitbreiding
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 447);
+            this.Controls.Add(this.txtRekening);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lstRekeningen);
+            this.Controls.Add(this.lstKlanten);
+            this.Controls.Add(this.btnNieuweKlant);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnOpname);
+            this.Controls.Add(this.btnStorting);
+            this.Controls.Add(this.txtBedrag);
+            this.Controls.Add(this.lblSaldo);
+            this.Controls.Add(this.lblNaam);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnNieuweRekening);
+            this.Controls.Add(this.txtAN);
+            this.Controls.Add(this.txtVN);
+            this.Controls.Add(this.label1);
+            this.Name = "InterfaceUitbreiding";
+            this.Text = "InterfaceUitbreiding";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpname;
+        private System.Windows.Forms.Button btnStorting;
+        private System.Windows.Forms.TextBox txtBedrag;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.Label lblNaam;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNieuweRekening;
+        private System.Windows.Forms.TextBox txtAN;
+        private System.Windows.Forms.TextBox txtVN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnNieuweKlant;
+        private System.Windows.Forms.ListBox lstKlanten;
+        private System.Windows.Forms.ListBox lstRekeningen;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtRekening;
+    }
+}
